c3e97c7bf639c58aaeb2ededdb7390046abd1152
Andreas Hirschauer <andi@linux.com>
2019-01-12 12:05:05 +0100

    Add voltastream files

---
+++ board/polyvection/voltastream0/voltastream0.c
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/arch/clock.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/mx6-pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/io.h>
+#include <common.h>
+#include <fsl_esdhc.h>
+#include <linux/sizes.h>
+#include <mmc.h>
+#include <command.h>
+#include <console.h>
+#include <fuse.h>
+#include <linux/errno.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define UART_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |		\
+	PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+int dram_init(void)
+{
+	gd->ram_size = imx_ddr_size();
+
+	return 0;
+}
+
+static iomux_v3_cfg_t const uart1_pads[] = {
+	MX6_PAD_UART1_TX_DATA__UART1_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX6_PAD_UART1_RX_DATA__UART1_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static void setup_iomux_uart(void)
+{
+	imx_iomux_v3_setup_multiple_pads(uart1_pads, ARRAY_SIZE(uart1_pads));
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return 0;
+}
+
+int mmc_map_to_kernel_blk(int devno)
+{
+	return devno;
+}
+
+int board_early_init_f(void)
+{
+	setup_iomux_uart();
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* Address of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM + 0x100;
+
+	return 0;
+}
+
+#ifdef CONFIG_CMD_BMODE
+static const struct boot_mode board_boot_modes[] = {
+	/* 4 bit bus width */
+	{"sd1", MAKE_CFGVAL(0x42, 0x20, 0x00, 0x00)},
+	{"sd2", MAKE_CFGVAL(0x40, 0x28, 0x00, 0x00)},
+	{NULL,	 0},
+};
+#endif
+
+int board_late_init(void)
+{
+#ifdef CONFIG_CMD_BMODE
+	add_board_boot_modes(board_boot_modes);
+#endif
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+	env_set("board_name", "VS");
+	env_set("board_rev", "A1");
+	env_set("uboot_ver", "2");
+
+	u32 bank, word, val;
+	int ret;
+	bank=4;
+	word=7;
+	ret = fuse_read(bank, word, &val);
+	if (ret) goto err;
+	
+	if (val == 00000005){
+		env_set("fdt_file", "/boot/imx6ull-voltastream-amp1.dtb");}
+	else if (val == 00000004){
+		env_set("fdt_file", "/boot/imx6ull-voltastream0.dtb");}
+	else if (val == 00000003){
+		env_set("fdt_file", "/boot/imx6ull-voltastream0.dtb");}
+	else if (val == 00000002){
+		env_set("fdt_file", "/boot/imx6ull-voltastream0.dtb");}
+	else if (val == 00000001){
+		env_set("fdt_file", "/boot/imx6ull-voltastream0.dtb");}
+    else if (val == 00000006){
+        env_set("fdt_file", "/boot/imx6ull-vsm1-0006.dtb");}
+	else {
+		env_set("fdt_file", "/boot/imx6ull-voltastream0.dtb");}
+
+	return 0;
+
+err:
+	puts("ERROR while reading fuses!\n");
+	return CMD_RET_FAILURE;
+
+#endif
+
+	return 0;
+}
+
+int checkboard(void)
+{
+	u32 bank, word, val;
+	int ret;
+	bank=4;
+	word=7;
+	ret = fuse_read(bank, word, &val);
+	if (ret) goto err;
+	
+	if (val == 00000005)
+		puts("Board: VS AMP1\n");
+	else if (val == 00000004)
+		puts("Board: VS ZERO - RAM:8G DAC:42\n");
+	else if (val == 00000003)
+		puts("Board: VS ZERO - RAM:8G DAC:21\n");
+	else if (val == 00000002)
+		puts("Board: VS ZERO - RAM:4G DAC:42\n");
+	else if (val == 00000001)
+		puts("Board: VS ZERO - RAM:4G DAC:21\n");
+    else if (val == 00000006)
+        puts("Board: VS M1 - ID: 0006\n");
+	else
+		puts("Board: Unknown\n");
+
+	return 0;
+
+err:
+	puts("ERROR while reading fuses!\n");
+	return CMD_RET_FAILURE;
+}
+
---
+++ include/configs/voltastream0.h
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for VoltaStream ZERO.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __VOLTASTREAM0_CONFIG_H
+#define __VOLTASTREAM0_CONFIG_H
+
+
+#include <asm/arch/imx-regs.h>
+#include <linux/sizes.h>
+#include "mx6_common.h"
+#include <asm/mach-imx/gpio.h>
+
+#ifdef CONFIG_SECURE_BOOT
+#ifndef CONFIG_CSF_SIZE
+#define CONFIG_CSF_SIZE 0x4000
+#endif
+#endif
+
+#define PHYS_SDRAM_SIZE	SZ_512M
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(16 * SZ_1M)
+
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE		UART1_BASE
+
+/* MMC Configs */
+#ifdef CONFIG_FSL_USDHC
+#define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC2_BASE_ADDR
+
+/* NAND pin conflicts with usdhc2 */
+#ifdef CONFIG_SYS_USE_NAND
+#define CONFIG_SYS_FSL_USDHC_NUM    1
+#else
+#define CONFIG_SYS_FSL_USDHC_NUM    2
+#endif
+#endif
+
+
+/* I2C configs */
+#ifdef CONFIG_CMD_I2C
+#define CONFIG_SYS_I2C_MXC
+#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
+#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_SPEED		100000
+#endif
+
+#define CONFIG_SYS_MMC_IMG_LOAD_PART	1
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"fdt_high=0xffffffff\0" \
+	"initrd_high=0xffffffff\0" \
+	"mmcautodetect=yes\0" \
+	"swbank=2\0" \
+	"switch= if test ${swbank} -eq 1; then " \
+			"setenv swbank 2; saveenv; " \
+		"else " \
+			"setenv swbank 1; saveenv; " \
+		"fi; " \
+		"echo swbank=${swbank};\0" \
+	"mmcargs=setenv bootargs console=ttymxc0,115200 " \
+		"root=/dev/mmcblk1p${swbank} coherent_pool=4M net.ifnames=0 rootwait rw\0" \
+	"loadimage=ext4load mmc 1:${swbank} 0x80800000 /boot/zImage\0" \
+	"loadfdt=ext4load mmc 1:${swbank} 0x83000000 ${fdt_file}\0" \
+	"mmcboot=echo Booting from swbank=${swbank} ...; " \
+		"run mmcargs; " \
+		"if run loadfdt; then " \
+			"bootz 0x80800000 - 0x83000000; " \
+		"else " \
+			"echo WARN: Cannot load the DT - ABORTING; " \
+		"fi; " \
+
+#define CONFIG_BOOTCOMMAND \
+	   "mmc dev 1;" \
+	   "mmc dev 1;" \
+	   "if mmc rescan; then " \
+           	"if run loadimage; then " \
+	   		"run mmcboot; " \
+		"else " \
+			"echo No kernel found on try 1- switching bank!; " \
+			"run switch; " \
+           	"fi; " \
+	   "fi;" \
+	   "mmc dev 1;" \
+	   "mmc dev 1;" \
+	   "if mmc rescan; then " \
+           	"if run loadimage; then " \
+	   		"run mmcboot; " \
+		"else " \
+			"echo No kernel found on try 2 - switching bank!; " \
+			"run switch; " \
+           	"fi; " \
+	   "fi;" \
+	   "mmc dev 1;" \
+	   "mmc dev 1;" \
+	   "if mmc rescan; then " \
+           	"if run loadimage; then " \
+	   		"run mmcboot; " \
+		"else " \
+			"echo No kernel found on try 3 - switching bank!; " \
+			"run switch; " \
+           	"fi; " \
+	   "fi;" \
+	   "mmc dev 1;" \
+	   "mmc dev 1;" \
+	   "if mmc rescan; then " \
+           	"if run loadimage; then " \
+	   		"run mmcboot; " \
+		"else " \
+			"echo No kernel found on try 4 - ROOTFS CORRUPTED!; " \
+           	"fi; " \
+	   "fi;"
+
+/* Miscellaneous configurable options */
+#define CONFIG_SYS_MEMTEST_START	0x80000000
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x8000000)
+
+#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
+#define CONFIG_SYS_HZ			1000
+
+/* Physical Memory Map */
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+/* environment organization */
+#define CONFIG_SYS_MMC_ENV_DEV		1	/* USDHC2 */
+#define CONFIG_SYS_MMC_ENV_PART		0	/* user area */
+#define CONFIG_MMCROOT			"/dev/mmcblk1p2"  /* USDHC2 */
+
+#define CONFIG_ENV_SIZE			SZ_8K
+#define CONFIG_ENV_OFFSET		(12 * SZ_64K)
+
+#define CONFIG_IMX_THERMAL
+
+#define CONFIG_IOMUX_LPSR
+
+#define CONFIG_SOFT_SPI
+
+#ifdef CONFIG_FSL_QSPI
+#define CONFIG_SYS_FSL_QSPI_AHB
+#define CONFIG_SF_DEFAULT_BUS       0
+#define CONFIG_SF_DEFAULT_CS        0
+#define CONFIG_SF_DEFAULT_SPEED 40000000
+#define CONFIG_SF_DEFAULT_MODE      SPI_MODE_0
+#define FSL_QSPI_FLASH_NUM      1
+#define FSL_QSPI_FLASH_SIZE     SZ_32M
+#endif
+
+#endif
